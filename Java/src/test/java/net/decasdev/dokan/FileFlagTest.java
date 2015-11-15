bd013cb12216ce341e9e26d6ec03fb37cf0da690
Sam Silverberg <sam.silverberg@gmail.com>
2015-11-15 15:15:00 -0800

    Updates for 8.0rc3

--- Java/src/test/java/net/decasdev/dokan/FileFlagTest.java
+++ Java/src/test/java/net/decasdev/dokan/FileFlagTest.java
@@ -8,29 +8,14 @@
 
 package net.decasdev.dokan;
 
-import junit.framework.Assert;
-import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.util.EnumSet;
 
-import net.decasdev.dokan.FileFlag.FileFlags;
+import org.junit.Test;
 public class FileFlagTest {
 
-    private static Logger logger = LoggerFactory.getLogger(FileFlagTest.class);
-
+    
     @Test
     public void getFlagsTest(){
-        int value = 0x02000000;
-        EnumSet<FileFlags> flags = FileFlag.getFlags(value);
-        Assert.assertTrue(flags.contains(FileFlags.FILE_FLAG_BACKUP_SEMANTICS));
-        Assert.assertFalse(flags.contains(FileFlags.FILE_FLAG_DELETE_ON_CLOSE));
-
-        value = 0x02000000 | 0x04000000;
-        flags = FileFlag.getFlags(value);
-        Assert.assertTrue(flags.contains(FileFlags.FILE_FLAG_BACKUP_SEMANTICS));
-        Assert.assertTrue(flags.contains(FileFlags.FILE_FLAG_DELETE_ON_CLOSE));
-        logger.debug("value = " + value + ": " +FileFlag.toString(value));
+        
 
     }
 
